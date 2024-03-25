5789d2fbca54fa59c44e05337751ef183edca923
Jiri Rejman <rejman@cngroup.dk>
2024-03-25 16:20:42 +0100

    fixed script fixed md

--- content/articles/aws/application-load-balancer.md
+++ content/articles/aws/application-load-balancer.md
@@ -8,6 +8,6 @@ draft = false
 ## Application load balancer
 Works on layer 7, routes to IP addresses, lambdas and containers. We can assign a security group to it. It routes to a target group either by path, based on url or host which is based on a host field in the header of http requests. 
 
-- [Load Balancer]({{<ref "./load-balancer>"}})
+- [Load Balancer]({{<ref "./load-balancer">}})
 - [EC2]({{<ref "./ec2">}})
 
--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,37 @@
 console.log("loaded");
-function sortList(ul) {
-  const ul = document.getElementById(ul);
 
-  Array.from(ul.getElementsByTagName("LI"))
+const sortByNewest = () => {
+  const ul = document.getElementById("books");
+
+  Array.from(ul.getElementsByClassName("bookItem"))
     .sort((a, b) => {
-      console.log("a");
-      return a.textContent.localeCompare(b.textContent);
+      const number = a.id;
+      const rating2 = b.id;
+      return rating2 - number;
     })
     .forEach((li) => ul.appendChild(li));
-}
+};
 
-console.log("here");
-sortList("books");
+const sortByRating = (name) => {
+  const ul = document.getElementById(name);
+
+  Array.from(ul.getElementsByClassName("bookItem"))
+    .sort((a, b) => {
+      const rating = Number(
+        a
+          .getElementsByClassName("rating")[0]
+          .textContent.split(" ")[1]
+          .split("/")[0]
+      );
+
+      const rating2 = Number(
+        b
+          .getElementsByClassName("rating")[0]
+          .textContent.split(" ")[1]
+          .split("/")[0]
+      );
+
+      return rating2 - rating;
+    })
+    .forEach((li) => ul.appendChild(li));
+};
